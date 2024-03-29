ac8bb08b4ab4579defdfb439900167632a0273fc
samandargofurov <samandargofurov236@gmail.com>
2024-03-29 12:38:11 +0500

    9 lesson

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Hero from './components/Hero/hero'
+import Navbar from './components/Navbar/navbar'
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <Hero></Hero>
+    </>
+  )
+}
+
+export default App
